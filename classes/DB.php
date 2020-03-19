4ba03187f524f9404558db9c336913ba53b1c7de
unknown <bluhanfi@gmail.com>
2020-03-19 18:27:36 +0200

    tmam

---
+++ classes/DB.php
@@ -0,0 +1,18 @@
+<?php
+class DB{
+
+        private static function connect(){
+  $pdo=new PDO('mysql:host=localhost;dbname=SocialNetwork;charset=utf8','root','');
+  $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+  return $pdo;
+                }
+//to interact with the database
+     public static function query($query, $params=array()){
+  $statement=self::connect()->prepare($query);
+  $statement->execute($params);
+  //$data=$statement->fetchAll();
+  //return $data;
+           }
+}
+
+?>
