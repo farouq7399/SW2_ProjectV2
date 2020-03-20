6f80c2615627f0d8e53216f21508c58359d11903
farouq7399 <39464976+farouq7399@users.noreply.github.com>
2020-03-20 02:52:33 +0200

    video 3 validation form

--- classes/DB.php
+++ classes/DB.php
@@ -2,16 +2,18 @@
 class DB{
 
         private static function connect(){
-  $pdo=new PDO('mysql:host=localhost;dbname=SocialNetwork;charset=utf8','root','');
-  $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-  return $pdo;
-                }
-//to interact with the database
+            $pdo=new PDO('mysql:host=localhost;dbname=SocialNetwork;charset=utf8','root','');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+          }
+        //to interact with the database
      public static function query($query, $params=array()){
-  $statement=self::connect()->prepare($query);
-  $statement->execute($params);
-  //$data=$statement->fetchAll();
-  //return $data;
+              $statement=self::connect()->prepare($query);
+              $statement->execute($params);
+              if(explode(' ',$query)[0] =='SELECT'){
+              $data=$statement->fetchAll();
+              return $data;
+              }
            }
 }
 
--- create-account.php
+++ create-account.php
@@ -6,8 +6,26 @@ if (isset($_POST['createaccount'])) {
    $username=$_POST['username'];
    $password=$_POST['password'];
    $email=$_POST['email'];
-   DB::query('INSERT INTO users VALUES (\'\',:username,:password,:email)',array(':username'=>$username,':password'=>$password,':email'=>$email));
-   echo "Success!";
+
+    if(!DB::query('SELECT username FROM users WHERE username=:username',array(':username' => $username ))){
+
+    		if(strlen($username)>=3 && strlen($username) <= 32){
+				 if ((preg_match('/[a-zA-z0-9_]+/', $username))) {
+					 if(filter_var($email,FILTER_VALIDATE_EMAIL)){
+					 	if(strlen($password)>= 6 && strlen($password) <=60){
+
+					 		DB::query('INSERT INTO users VALUES (\'\',:username,:password,:email)',array(':username'=>$username,':password'=>password_hash( $password,PASSWORD_BCRYPT),':email'=>$email));
+					 	}else{echo "Password Invalid ";}
+
+					   echo "Success!";
+					}else{echo "invalid email";}
+				 }else{echo "invalid user name ";}
+			}else{echo "invalid user name";}
+
+		}
+		else{
+			echo "User Already Exists!";
+		}
 }
 
  ?>
