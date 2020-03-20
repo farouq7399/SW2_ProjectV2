a745608f6e25057105ff2fe010e4ba835e4f836d
unknown <bluhanfi@gmail.com>
2020-03-20 23:49:23 +0200

    thats how u change password

--- change-password.php
+++ change-password.php
@@ -1,51 +1,76 @@
 <?php
 include('./classes/DB.php');
 include('./classes/Login.php');
-
-
-//i have to be logged in to change password
+$tokenIsValid = False;
 if (Login::isLoggedIn()) {
-//when press change password button
-    if (isset($_POST['changepassword'])) {
-      //get what i typed in the form and access it to the database
-      $oldpassword=$_POST['oldpassword'];
-      $newpassword=$_POST['newpassword'];
-      $newpasswordrepeat=$_POST['newpasswordrepeat'];
-      //easier to handle more than username its unique
-      $userid = Login::isLoggedIn();
-      //first see if the password is correct
-        if (password_verify($oldpassword, DB::query('SELECT password FROM users WHERE id=:userid', array(':userid'=>$userid))[0]['password'])){
-           //see if the password repeated correctly
-           if ($newpassword==$newpasswordrepeat) {
-             //our normal pass constraint
-              if(strlen($newpassword)>= 6 && strlen($newpassword) <=60){
-                  //now we passed all the constraints now change the password
-                 DB::query('UPDATE users SET password=:newpassword WHERE id=:userid',array(':newpassword'=>password_hash( $newpassword,PASSWORD_BCRYPT),':userid'=>$userid));
-                   echo "Password changed successfully! ";
-              }
-           } else {
-             echo "passwords dont match !";
-           }
+
+        if (isset($_POST['changepassword'])) {
+
+                $oldpassword = $_POST['oldpassword'];
+                $newpassword = $_POST['newpassword'];
+                $newpasswordrepeat = $_POST['newpasswordrepeat'];
+                $userid = Login::isLoggedIn();
+
+                if (password_verify($oldpassword, DB::query('SELECT password FROM users WHERE id=:userid', array(':userid'=>$userid))[0]['password'])) {
+
+                        if ($newpassword == $newpasswordrepeat) {
+
+                                if (strlen($newpassword) >= 6 && strlen($newpassword) <= 60) {
+
+                                        DB::query('UPDATE users SET password=:newpassword WHERE id=:userid', array(':newpassword'=>password_hash($newpassword, PASSWORD_BCRYPT), ':userid'=>$userid));
+                                        echo 'Password changed successfully!';
+                                }
+
+                        } else {
+                                echo 'Passwords don\'t match!';
+                        }
+
+                } else {
+                        echo 'Incorrect old password!';
+                }
+
         }
 
-        else {
+} else {
+        if (isset($_GET['token'])) {
+        $token = $_GET['token'];
+        if (DB::query('SELECT user_id FROM password_tokens WHERE token=:token', array(':token'=>sha1($token)))) {
+                $userid = DB::query('SELECT user_id FROM password_tokens WHERE token=:token', array(':token'=>sha1($token)))[0]['user_id'];
+                $tokenIsValid = True;
+                if (isset($_POST['changepassword'])) {
 
-          echo "Incorrect old password! ";
+                        $newpassword = $_POST['newpassword'];
+                        $newpasswordrepeat = $_POST['newpasswordrepeat'];
+
+                                if ($newpassword == $newpasswordrepeat) {
+
+                                        if (strlen($newpassword) >= 6 && strlen($newpassword) <= 60) {
+
+                                                DB::query('UPDATE users SET password=:newpassword WHERE id=:userid', array(':newpassword'=>password_hash($newpassword, PASSWORD_BCRYPT), ':userid'=>$userid));
+                                                echo 'Password changed successfully!';
+                                                DB::query('DELETE FROM password_tokens WHERE user_id=:userid', array(':userid'=>$userid));
+                                        }
+
+                                } else {
+                                        echo 'Passwords don\'t match!';
+                                }
+
+                        }
+
+
+        } else {
+                die('Token invalid');
         }
-    }
-
-
+} else {
+        die('Not logged in');
+}
 }
 
-else {
-      die('not logged in');
-}
-//normal form to test this 
 ?>
-<h1>Change password</h1>
-<form action="change-password.php" method="post">
-  <input type="password" name="oldpassword" value="" placeholder="Current Password ..."><br /><p />
-  <input type="password" name="newpassword" value="" placeholder="New Password ..."><br /><p />
-  <input type="password" name="newpasswordrepeat" value="" placeholder="Repeat Password ..."><br /><p />
-  <input type="submit" name="changepassword" value="Change Password">
+<h1>Change your Password</h1>
+<form action="<?php if (!$tokenIsValid) { echo 'change-password.php'; } else { echo 'change-password.php?token='.$token.''; } ?>" method="post">
+        <?php if (!$tokenIsValid) { echo '<input type="password" name="oldpassword" value="" placeholder="Current Password ..."><p />'; } ?>
+        <input type="password" name="newpassword" value="" placeholder="New Password ..."><p />
+        <input type="password" name="newpasswordrepeat" value="" placeholder="Repeat Password ..."><p />
+        <input type="submit" name="changepassword" value="Change Password">
 </form>
