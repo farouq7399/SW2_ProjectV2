683c899c74b0b869ad1c145c66f5e68b2ed0f6c1
Mohamed Emad <38816824+MohamedEmad87@users.noreply.github.com>
2020-04-12 01:23:52 +0200

    Adding registration page with vuejs validation.

---
+++ js/website.js
@@ -0,0 +1,56 @@
+var vm = new Vue({
+  el: "#emad-app",
+  data: {
+    fieldType: "password",
+    formErrors: [],
+    userName: null,
+    password: null,
+    email: null,
+    maxChars: 15,
+    minChars: 8,
+  },
+  methods: {
+    switchField() {
+      this.fieldType = this.fieldType === "password" ? "text" : "password";
+      // first this.fieldType refers to filedType property
+    },
+    validateForm: function (e) {
+      this.formErrors = []; // Empty errors to start fresh
+      // [1] Check if username is empty
+      if (!this.userName) {
+        this.formErrors.push("Username Can't be empty");
+      }
+      // [2] Check if password is empty
+      if (!this.password) {
+        this.formErrors.push("Password Can't be empty");
+      }
+      // [3] Check username characters count
+      if (this.userName && this.userName.length > this.maxChars) {
+        this.formErrors.push(
+          `Username can't be more than ${this.maxChars} chracters`
+        );
+      }
+      // [4] Check password characters count
+      if (this.password && this.password.length < this.minChars) {
+        this.formErrors.push(
+          `Username can't be less than ${this.minChars} chracters`
+        );
+      }
+      // [5] Check email validation
+      if (this.email) {
+        if (!this.validEmail(this.email)) {
+          this.formErrors.push("Valid email required");
+        }
+      }
+      // If no errors return true
+      if (!this.formErrors.length) {
+        return true;
+      }
+      e.preventDefault();
+    },
+    validEmail: function (email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    },
+  },
+});
